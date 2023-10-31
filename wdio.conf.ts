a481f8e70cc30abfb5266c49a5979d6f0fd72029
Akhil <akhil.gupta@capco.com>
2023-10-31 09:37:24 +0530

    set browser binary

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -71,6 +71,8 @@ export const config: Options.Testrunner = {
         // 'goog:chromeOptions': {
         //     args: ['--headless']
         // }
+        'goog:chromeOptions': { 
+        binary: 'C:\\Users\\AGPX\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'}
     }],
 
     //
